08dbc3b7c172dbf7168b070ea95e2a99e702ccb8
Sergey Masiuk <mayerror@gmail.com>
2024-02-10 21:25:01 +0300

    feat: add simple test functionality

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -8,26 +8,36 @@ describe('simpleCalculator tests', () => {
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: Action.Subtract });
+    expect(result).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: Action.Multiply });
+    expect(result).toBe(15);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: Action.Add });
+    expect(result).toBe(8);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    });
+    expect(result).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 5, b: 3, action: '%' });
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result = simpleCalculator({ a: 'invalid', b: 3, action: Action.Add });
+    expect(result).toBeNull();
   });
 });
