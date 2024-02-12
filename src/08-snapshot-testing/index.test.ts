12b27fd9235250284ddfed8e7f528e623ff63d4f
Sergey Masiuk <mayerror@gmail.com>
2024-02-12 22:44:46 +0300

    fix: delete extra console.log

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -6,14 +6,12 @@ describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
     const list = generateLinkedList([5]);
-    console.log(list);
     expect(list).toStrictEqual(testList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     const list = generateLinkedList([5, 2]);
-    console.log(list);
     expect(list).toMatchSnapshot();
   });
 });
